4246fbce629d305d897b2ea6188bf1dafb96d8b3
samigumus <abdullahsamigumus@gmail.com>
2019-11-20 14:39:44 -0500

    send mail

---
+++ src/test/java/Gmail/Credentials.java
@@ -0,0 +1,14 @@
+package Gmail;
+
+public class Credentials {
+
+        private String userName = "";
+        private String passWord = "";
+        public String getUserName(){
+            return userName;
+        }
+        public String getPassWord(){
+            return passWord;
+        }
+
+}
---
+++ src/test/java/Gmail/SendEmails.java
@@ -0,0 +1,42 @@
+package Gmail;
+
+import org.apache.commons.mail.SimpleEmail;
+public class SendEmails {
+    /*
+         first Step:
+             go to  :
+                  https://myaccount.google.com/lesssecureapps?utm_source=google-account&utm_medium=web
+                     Make sure that your google account allows third part app
+      */
+    public static void main(String[] args) {
+        Credentials obj = new Credentials();
+        String userName = obj.getUserName(),
+                passWord = obj.getPassWord(),
+                MSG ="",
+                Subject ="";
+        String[] Receivers = {""};
+        int times = 1;
+        while( times > 0) {
+            for (String students : Receivers) {
+                sendEmails(userName, passWord, students, MSG, Subject);
+                System.out.println("Send successfully");
+            }
+            times--;
+        }
+    }
+    public static void sendEmails(String username, String password, String Receiver, String MSG, String Subject){
+        SimpleEmail email=new SimpleEmail();
+        email.setHostName("smtp.googlemail.com");
+        email.setSmtpPort(465);
+        email.setAuthentication(username, password);
+        email.setSSLOnConnect(true);
+        email.setSubject(Subject);
+        try {
+            email.setFrom(username);
+            email.setMsg(MSG);
+            email.addTo(Receiver);
+            email.send();
+            Thread.sleep(3000);
+        } catch (Exception e) {   }
+    }
+}
